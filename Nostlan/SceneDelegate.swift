7bd814e6fab0d7cf60518d4d0dbb1463b0f64aeb
Quinton Ashley <quintonashley5@gmail.com>
2020-04-27 02:14:26 -0400

    0.2.2

--- Nostlan/SceneDelegate.swift
+++ Nostlan/SceneDelegate.swift
@@ -19,11 +19,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 		// If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
 		// This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
 
-		// https://jsnes.org/
 		// https://html5gamepad.com/
 		// https://tsone.kapsi.fi/em-fceux/
 		// Create the SwiftUI view that provides the window contents.
-		let webView = ContentView(pageURL: "https://jsnes.org/");
+		let webView = ContentView(pageURL: "https://tsone.kapsi.fi/em-fceux/");
 
 		// Use a UIHostingController as window root view controller.
 		if let windowScene = scene as? UIWindowScene {
